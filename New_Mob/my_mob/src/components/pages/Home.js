12558980105669af71392202dcd9b879ab0fb8b6
MamaKbubu <maboelatk@gmail.com>
2024-03-01 22:59:29 +0200

    my project

--- New_Mob/my_mob/src/components/pages/Home.js
+++ New_Mob/my_mob/src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../App.css";
 import HeroSection from "../HeroSection";
-import { Outlet } from "react-router-dom";
 import Cards from "../Cards";
 
 function Home() {
@@ -9,7 +8,6 @@ function Home() {
     <>
       <HeroSection />
       <Cards />
-      <Outlet />
     </>
   );
 }
